a24532cae4ac8e9221aab4b7e3f2cc675228a247
khanhduy241 <57337421+khanhduy241@users.noreply.github.com>
2019-11-09 15:34:24 +0700

    update blur gaussian

--- Blur.h
+++ Blur.h
@@ -33,3 +33,33 @@ Blur::Blur()
 Blur::~Blur()
 {}
 
+int Blur::BlurImage(const Mat& sourceImage, Mat& destinationImage, int kWidth, int kHeight, int method)
+{
+	// lam mo bang toan tu Gaussian
+	if (method == 2)
+	{
+		double size = (double)kWidth * (double)kHeight;
+		vector<float> kernel;
+		float sigma = 1;
+
+		// tao kernel
+		for (int i = 0; i < size; i++)
+		{
+			int row = i / kWidth - kWidth / 2;
+			int col = i % kHeight - kHeight / 2;
+			float h = exp(-(float)(col * col + row * row) / (float)(2 * sigma * sigma)) / (sqrt(2 * 3.1415) * sigma);
+			kernel.push_back(h);
+		}
+
+		// thuc hien tich chap
+		Convolution gauss;
+		gauss.SetKernel(kernel, kWidth, kHeight);
+
+		gauss.DoConvolution(sourceImage, destinationImage);
+
+		if (sourceImage.empty() || destinationImage.empty())
+			return 0;
+		else
+			return 1;
+	}
+}
