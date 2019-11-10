31eccfb55d8efdd2a3b82ce1c62aec6f43418075
azzen-K <nguyenanhkhoa301199@gmail.com>
2019-11-10 12:36:03 +0700

    Add quick sort to median filter

--- Blur.h
+++ Blur.h
@@ -33,6 +33,35 @@ Blur::Blur()
 
 Blur::~Blur()
 {}
+int partition(vector<int>& values, int left, int right) {
+	int pivotIndex = left + (right - left) / 2;
+	int pivotValue = values[pivotIndex];
+	int i = left, j = right;
+	int temp;
+	while (i <= j) {
+		while (values[i] < pivotValue) {
+			i++;
+		}
+		while (values[j] > pivotValue) {
+			j--;
+		}
+		if (i <= j) {
+			temp = values[i];
+			values[i] = values[j];
+			values[j] = temp;
+			i++;
+			j--;
+		}
+	}
+	return i;
+}
+void quicksort(vector<int>& values, int left, int right) {
+	if (left < right) {
+		int pivotIndex = partition(values, left, right);
+		quicksort(values, left, pivotIndex - 1);
+		quicksort(values, pivotIndex, right);
+	}
+}
 
 int Blur::BlurImage(const Mat& sourceImage, Mat& destinationImage, int kWidth, int kHeight, int method)
 {
